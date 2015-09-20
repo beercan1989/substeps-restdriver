e1a8ec49e1aefbc7a636327216574c50370d5953
James Bacon <james@baconi.co.uk>
2015-09-20 13:06:18 +0100

    Updating README with what's been added so far for the 0.0.1 release.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# substeps-restdriver
+# Substeps RestDriver
+Project to provide a new type of Driver for use with Substeps, that will enable the ability to testing of rest endpoints by creating requests and asserting the responses.
+
++ Substeps Site: https://substeps.g2g3.digital
++ Substeps Sources: https://github.com/G2G3Digital?utf8=%E2%9C%93&query=substeps
+
+## Basic Requirements
++ Java 8
++ Substeps 1.1.2
+
+## Features coming up in 0.0.1
++ Build rest requests.
++ Add headers in key value pairs.
++ Add cookies in key value pairs.
++ Add data to send in key/value pairs
++ Choose either a simple JSON or Key Pair builders to produce a suitable request body.
++ Set connect timeout, overriding defaults.
++ Set socket timeout, overriding defaults.
++ Set user agent, overriding defaults.
++ Defaults are stored in Typesafe Config properties.
++ Basic rest assertions:
+    + Status Code
+    + Status Code in Range
+    + Reason Phrase
++ Basic JSON finding via JsonPath.
++ Basic JSON asserting via JsonPath and very basic types.
+    + Object - only that it is one, haven't worked out best way to assert contents. 
+    + Array - only that it is one, haven't worked out best way to assert contents.
+    + String - both finding and matching, ignoring whitespace
+    + Number - both finding and matching, including decimals
++ Examples / documentation on how to use.
+
+## Useful information
++ Scopes - can be the following values, they are thread safe scopes where data can be stored during substep runs:
+    + SUITE
+    + FEATURE
+    + SCENARIO
+    + SCENARIO_BACKGROUND
+    + SCENARIO_OUTLINE
+    + SCENARIO_OUTLINE_ROW
+    + STEP
---
+++ do-release-without-deploying-artifacts.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+. "$(git --exec-path)/git-sh-setup"
+
+## Make sure no changes are outstanding.
+require_clean_work_tree "perform a release"
+
+## Make sure we are at the top of the repository
+cd_to_toplevel
+
+function getCurrentPomVersion {
+  mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.4.0:exec 2> /dev/null
+}
+
+function incrementPomVersion {
+  
+  read currentPomVersion
+  
+	declare -a version=($(echo ${currentPomVersion} | tr - . | tr . ' '))
+	
+	if [ "$2" == "major" ]; then
+		## [THIS].0.1-SNAPSHOT
+		version[0]=$[${version[0]} + 1]
+	elif [ "$2" == "minor" ]; then
+		## 0.[THIS].1-SNAPSHOT
+		version[1]=$[${version[1]} + 1]
+	else
+		## 0.0.[THIS]-SNAPSHOT
+		version[2]=$[${version[2]} + 1]
+	fi
+	
+	if [ -n ${version[3]} ]; then
+		version[3]=-${version[3]}
+	fi
+	
+	echo ${version[0]}.${version[1]}.${version[2]}${version[3]}
+}
+
+CURRENT_SNAP_VERSION=$(getCurrentPomVersion)
+RELEASE_VERSION=${CURRENT_SNAP_VERSION%-SNAPSHOT}
+NEXT_SNAP_VERSION=$(echo ${CURRENT_SNAP_VERSION} | incrementPomVersion)
+
+echo "Current Version: ${CURRENT_SNAP_VERSION}"
+echo "Release Version: ${RELEASE_VERSION}"
+echo "Next Version:    ${NEXT_SNAP_VERSION}"
+
+## Remove SNAPSHOT
+## Compile and Install
+## Commit Change and Tag
+## Increment, add SNAPSHOT and Commit Change
+mvn --batch-mode release:prepare -Dtag="${RELEASE_VERSION}" -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${NEXT_SNAP_VERSION}" -DautoVersionSubmodules=true 
+
+## Clean Up Local Repo
+find -type f -name '*.releaseBackup' | xargs -I {} rm -v {}
+rm -v release.properties
